52d249ef07fad9bad612942801af0ef5a6194c67
Shiheng-Wang <shihengwang@umail.ucsb.edu>
2021-03-01 13:16:21 -0800

    Update evaluate.cpp

--- evaluate.cpp
+++ evaluate.cpp
@@ -6,62 +6,44 @@
 
 int budget;
 
-bool myCompare(std::pair<std::string, float> a, std::pair<std::string, float> b) {
-    return a.second > b.second;
+struct Stock{
+    std::string name;
+    float Yvalue;
+    float openPrice;
+};
+
+bool myCompare(Stock a, Stock b) {
+    return a.Yvalue > b.Yvalue;
 }
 
 int main(int argc, char* argv[]) {
-    if (argc < 3) {
-        std::cerr << "Enter year and month as command line arguments.\n";
-        exit(0);
-    }
 
     std::cout << "Enter your budget: ";
     std::cin >> budget;
-
-    float actualY = std::numeric_limits<float>::min();
-    std::string maxName, line, month = argv[2], year = argv[1];
-    std::vector<std::pair<std::string, float> > actualRanking;
-
-    std::string filename = "Yvalue";
-    filename += year + month + ".csv";
+    std::string line;
 
     std::ifstream myFile;
-    myFile.open(filename);
-    float curY;
+    float curY, curPrice;
     size_t commaIndex;
     std::string name;
-    while (std::getline(myFile, line)) {
-        commaIndex = line.find(',');
-        curY = std::stof(line.substr(commaIndex + 1));
-        name = line.substr(0, commaIndex);
-        if (curY > actualY) {
-            actualY = curY;
-            maxName = name;
-        }
-        std::pair<std::string, float> curPair(name, curY);
-        actualRanking.push_back(curPair);
-    }
-
-    actualRanking.sort(actualRanking.begin(), actualRanking.end(), myCompare);
-
-    myFile.close();
-
-    std::cout << "Best stock this month is " << maxName << ", Y value = " << actualY << "\n";
 
     // Get output from the model
-    std::vector<std::pair<std::string, float> > modelOut;
-    myFile.open("resutl.txt");
+    std::vector<Stock> modelOut;
+    myFile.open("sample_output.csv");
+    std::getline(myFile, line);
     
     while (std::getline(myFile, line)) {
         commaIndex = line.find(',');
         curY = std::stof(line.substr(commaIndex + 1));
         name = line.substr(0, commaIndex);
-        std::pair<std::string, float> curPair(name, curY);
-        modelOut.push_back(curPair);
+        Stock cur;
+        cur.name = name;
+        cur.Yvalue = curY;
+        cur.openPrice = curPrice;
+        modelOut.push_back(cur);
     }
 
-    modelOut.sort(modelOut.begin(), modelOut.end(), myCompare);
+    std::sort(modelOut.begin(), modelOut.end(), myCompare);
     myFile.close();
 
     // Pick the top stocks to buy
@@ -69,10 +51,11 @@ int main(int argc, char* argv[]) {
     int numOfCurStock;
     std::vector<std::pair<std::string, int> > result;
     for (int i = 0; i < modelOut.size(); i++) {
-        numOfCurStock = budget / modelOut[i].second;
-        budget -= numOfCurStock * modelOut[i].second;
+        numOfCurStock = budget / modelOut[i].openPrice;
+        if (numOfCurStock == 0) continue;
+        budget -= numOfCurStock * modelOut[i].openPrice;
         std::pair<std::string, int> curStock;
-        curStock.first = modelOut[i].first;
+        curStock.first = modelOut[i].name;
         curStock.second = numOfCurStock;
         result.push_back(curStock);
     }
